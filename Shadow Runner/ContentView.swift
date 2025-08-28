0946d6a751cf9b2a2f16aefc936477377d359219
Nam Nguyễn <namnv@ermsystem.com>
2025-08-29 03:37:02 +0700

    Initial Commit

---
+++ Shadow Runner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shadow Runner
+//
+//  Created by Nam Nguyễn on 29/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shadow Runner/Shadow_RunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shadow_RunnerApp.swift
+//  Shadow Runner
+//
+//  Created by Nam Nguyễn on 29/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Shadow_RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
